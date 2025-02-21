7a32dd965078f577a8582a1557002cef0e739ff6
Kirill Churkin <st129364@student.spbu.ru>
2025-02-21 23:00:22 +0300

    Add a simple test file (base for fruture tests)

---
+++ tests/test.cpp
@@ -0,0 +1,12 @@
+/* Churkin Kirill st129364@student.spbu.ru
+	Lab_work_2
+*/
+
+
+#include <iostream>
+
+int main() 
+{
+    std::cout << "Test executable compiled and ran successfully!" << std::endl;
+    return 0;
+};
